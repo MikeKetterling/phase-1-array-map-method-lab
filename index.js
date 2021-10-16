bc91ed4fe193d7f0c319374dbf8ea6cfe8212a8a
Mike <mikekett4@yahoo.com>
2021-10-16 17:59:04 -0600

    commit tree 085490d00a1853e95148db571da920bf102c050a

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = string => {
+//   return tutorials
+// }
+
+// let title = 'what does the this keyword mean?'
+
+
+
+// const titleCased = string => {
+//   return string.toLowerCase().split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+
+// console.log(titleCased(title))
+
+const input = tutorials.map(uppercaseWords)
+
+function titleCased() {
+  return input;
+} 
+ 
+function uppercaseWords (array){
+  let newArray = array.split(" ");
+  for (let i = 0; i < newArray.length; i ++ ){
+    newArray[i] = newArray[i].charAt(0).toUpperCase() + newArray[i].slice(1);
+  }
+  return newArray.join(" ")
 }
